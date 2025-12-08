f654106510f4aaeb36a6106cb6772b1ac5b82a17
Aditya Majhi <majhi2723@gmail.com>
2025-12-08 17:38:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`DevPilot HQ server running on port ${PORT}`);
+});
+
+module.exports = app;
